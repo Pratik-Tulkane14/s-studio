bf2b0c52ba8709ff55ffe643c99887dba1289813
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-04-07 17:28:53 +0530

    added folder structure

--- script.js
+++ script.js
@@ -2,61 +2,97 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+const imageHover = () => {
+    const elemContainer = document.querySelector("#elem-container");
+    const fixedImg = document.querySelector(".fixed-image");
+    const elem = document.querySelectorAll(".elem");
+    elem.forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            let imageUrl = item.getAttribute("data-image")
+            fixedImg.style.display = "block"
+            fixedImg.style.backgroundImage = `url(${imageUrl})`
 
-
-function page4Animation() {
-    var elemC = document.querySelector("#elem-container")
-    var fixed = document.querySelector("#fixed-image")
-    elemC.addEventListener("mouseenter", function () {
-        fixed.style.display = "block"
+        });
     })
-    elemC.addEventListener("mouseleave", function () {
-        fixed.style.display = "none"
-    })
-
-    var elems = document.querySelectorAll(".elem")
-    elems.forEach(function (e) {
-        e.addEventListener("mouseenter", function () {
-            var image = e.getAttribute("data-image")
-            fixed.style.backgroundImage = `url(${image})`
-        })
+    elemContainer.addEventListener("mouseleave", () => {
+        fixedImg.style.display = "none"
     })
 }
 
-function swiperAnimation() {
+const swiperAnimation = () => {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
-        centeredSlides: true,
+        centeredSlides: false,
         spaceBetween: 100,
+        autoplay: window.innerWidth <= 768 ? {
+            delay: 3000,
+            disableOnInteraction: false,
+        } : true,
+        breakpoints: {
+            600: {
+                slidesPerView: 1,
+                centeredSlides: false,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                centeredSlides: true,
+                spaceBetween: 50,
+            },
+            1024: {
+                slidesPerView: 3,
+                // centeredSlides: true,
+                spaceBetween: 70,
+            },
+        }
     });
 }
-function menuAnimation() {
-
-    var menu = document.querySelector("nav h3")
-    var full = document.querySelector("#full-scr")
-    var navimg = document.querySelector("nav img")
-    var flag = 0
-    menu.addEventListener("click", function () {
-        if (flag == 0) {
-            full.style.top = 0
-            navimg.style.opacity = 0
-            flag = 1
+const menuToggler = () => {
+    const navImage = document.querySelector("nav img");
+    const menu = document.querySelector(".hamburger-wrapper");
+    const fullScr = document.querySelector("#full-scr");
+    let flag = true;
+    menu.addEventListener("click", () => {
+        if (flag) {
+            navImage.style.opacity = "0";
+            fullScr.style.top = "0";
+            flag = !flag;
         } else {
-            full.style.top = "-100%"
-            navimg.style.opacity = 1
-            flag = 0
+            navImage.style.opacity = "1";
+            fullScr.style.top = "-108%";
+            flag = !flag;
         }
     })
 }
-
-function loaderAnimation() {
-    var loader = document.querySelector("#loader")
-    setTimeout(function () {
-        loader.style.top = "-100%"
-    }, 4200)
+const hamburgerMenu = () => {
+    const hamburgerWrapper = document.querySelector(".hamburger-wrapper");
+    hamburgerWrapper.addEventListener("click", () => {
+        const hamburger = document.querySelectorAll(".hamburger");
+        hamburger.forEach((e) => {
+            e.classList.toggle("active")
+        })
+    })
 }
+const initialLoader = () => {
+    const loader = document.querySelector("#loader");
+    setTimeout(() => {
+        loader.style.top = "-100%";
+    }, 4200);
+}
+document.querySelector("form", addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("yes");
 
-swiperAnimation()
-page4Animation()
-menuAnimation()
-loaderAnimation()
+}))
+ document.addEventListener('contextmenu',(e)=>{
+    e.preventDefault()
+})
+
+const year = document.querySelector("#current-year");
+year.innerHTML = new Date().getFullYear();
+
+imageHover();
+swiperAnimation();
+menuToggler();
+hamburgerMenu();
+initialLoader();
